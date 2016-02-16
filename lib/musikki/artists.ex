c7388688b2c6da75ed589cbec709ff7d88c8173e
Sasha Fonseca <sashaafm@gmail.com>
2016-02-16 22:59:14 +0000

    Implemented about half of the Artists endpoint, but not all filters are working although the URL seems correct.

--- lib/musikki/artists.ex
+++ lib/musikki/artists.ex
@@ -27,5 +27,96 @@ defmodule Musikki.Artists do
 				@appKey
 			])  
 	end
+
+	def bio(mkid) do 
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/bio", 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])  		
+	end
+
+	def collaborations(mkid) do
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/collaborations", 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])
+	end
+
+	def collaborations(mkid, filters) do 
+		collabs mkid, filters, ""
+	end
+
+	defp collabs(mkid, [], args_list) do 
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/collaborations" 
+						<> "?q=" <> args_list, 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])				
+	end
+
+	defp collabs(mkid, [{param, value} | tail], args_list) do 
+		case tail do 
+			[] -> collabs mkid, tail, args_list <> "[#{param}:#{value}]"
+
+			_  -> collabs mkid, tail, args_list <> "[#{param}:#{value}],"
+		end
+	end
+
+	def labels(mkid) do 
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/labels", 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])		
+	end
+
+	def listen(mkid) do 
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/listen", 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])				
+	end
+
+	def news(mkid) do 
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/news", 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])				
+	end	
+
+	def related(mkid, type) do 
+		HTTPoison.request(
+			:get, 
+			@host <> @endpoint <> "/#{mkid}" <> "/related?q=[relation-type:#{type}]", 
+			"", 
+			[
+				@appID, 
+				@appKey
+			])			
+	end
+
  	
  end
